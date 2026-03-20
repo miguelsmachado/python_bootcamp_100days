7d50719dbea5b376e9bd13ec8306973e57708a25
Miguel <miguelsmachado@gmail.com>
2026-03-19 23:24:56 -0300

    fim do  dia 1

---
+++ day_001.py
@@ -0,0 +1,14 @@
+'''
+Programa que gera o nome de uma banda baseado no nome da cidade que você nasceu
+e em um tipo de animal.
+'''
+
+print("Welcome to the Band Name Generator")
+
+city = str(input("What's the name of the city you grew up in?\n"))
+
+animal = str(input("What's your pet name?\n"))
+
+print(f"Your band name could be {city} {animal}")
+
+
